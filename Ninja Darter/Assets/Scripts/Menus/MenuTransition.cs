43f7b52108e8d7276fb287091a47f4336968ab92
Fatih <maolud62@gmail.com>
2020-08-12 23:35:21 -0400

    Added Start screen and Options screen

---
+++ Ninja Darter/Assets/Scripts/Menus/MenuTransition.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class MenuTransition : MonoBehaviour {
+
+    [SerializeField] private GameObject _mainMenuObject;
+    [SerializeField] private GameObject _optionsObject;
+
+    public void SetMainMenuActive () {
+        _mainMenuObject.SetActive(true);
+        _optionsObject.SetActive(false);
+    }
+
+    public void SetMainOptionsActive () {
+        _mainMenuObject.SetActive(false);
+        _optionsObject.SetActive(true);
+    }
+    
+}
