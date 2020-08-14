2f9091d01cad51ec1c24a670d5bf61cfda17e9bb
Fatih <maolud62@gmail.com>
2020-08-14 17:29:54 -0400

    Added user interface; main menu and pause menu

--- Ninja Darter/Assets/Scripts/Menus/MenuTransition.cs
+++ Ninja Darter/Assets/Scripts/Menus/MenuTransition.cs
@@ -4,15 +4,42 @@ public class MenuTransition : MonoBehaviour {
 
     [SerializeField] private GameObject _mainMenuObject;
     [SerializeField] private GameObject _optionsObject;
+    [SerializeField] private GameObject _loadObject;
+    [SerializeField] private GameObject _newGameObject;
+
+    public void NewGameStart() {
+        // fade out slowly, then disable all objects
+        // load the next scene that starts the game
+    }
 
     public void SetMainMenuActive () {
         _mainMenuObject.SetActive(true);
         _optionsObject.SetActive(false);
+        _loadObject.SetActive(false);
+        _newGameObject.SetActive(false);
     }
 
     public void SetMainOptionsActive () {
         _mainMenuObject.SetActive(false);
         _optionsObject.SetActive(true);
+        _loadObject.SetActive(false);
+        _newGameObject.SetActive(false);
     }
+
+    public void SetMainLoadActive () {
+        _mainMenuObject.SetActive(false);
+        _optionsObject.SetActive(false);
+        _loadObject.SetActive(true);
+        _newGameObject.SetActive(false);
+    }
+
+    public void SetNewGameActive() {
+        _mainMenuObject.SetActive(false);
+        _optionsObject.SetActive(false);
+        _loadObject.SetActive(false);
+        _newGameObject.SetActive(true);
+    }
+
+    public void QuitGame() => Application.Quit();
     
 }
---
+++ Ninja Darter/Assets/Scripts/Menus/PauseMenu.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    [SerializeField] private GameObject _pauseMenu;
+    [SerializeField] private GameObject _optionsMenu;
+
+    
+    void Update() => CheckPauseKey();
+    
+    // check for Pause key or start button
+    void CheckPauseKey() {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            SetPauseOn();
+    }
+
+    public void SetPauseOn() {
+        _pauseMenu.SetActive(true);
+        _optionsMenu.SetActive(false);
+        if (Time.timeScale == 1.0f) Time.timeScale = 0.0f;
+    }
+
+    public void SetPauseOff() {
+        _pauseMenu.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+
+    public void OpenOptionsMenu() {
+        _pauseMenu.SetActive(false);
+        _optionsMenu.SetActive(true);
+    }
+
+    public void QuitGame() => Application.Quit();
+}
