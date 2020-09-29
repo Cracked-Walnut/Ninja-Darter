4cdf31f6768811c8b1a76de8657f5f7372f29e78
Fatih <maolud62@gmail.com>
2020-09-29 16:36:22 -0400

    Started work on player air attack

--- Ninja Darter/Assets/Scripts/Player/PlayerState.cs
+++ Ninja Darter/Assets/Scripts/Player/PlayerState.cs
@@ -26,18 +26,22 @@ public class PlayerState : MonoBehaviour {
 
     [Header("Health")]
     [SerializeField] private int _health;
-    [SerializeField] private LayerMask _whatIsEnemy; // determines what can damage the player
+    [SerializeField] private LayerMask _enemies; // determines what can damage the player
 
     [Header("Animator")]
     [SerializeField] private Animator _animator;
 
     [Header("Attacking")]
+    [SerializeField] private int _attackDamage = 50;
     [SerializeField] private bool _canAttack = true;
+    [SerializeField] private bool _canAirAttack = false;
     [SerializeField] private float _timeBtwAttacks; // a small delay between each attack
+    [SerializeField] private float _timeBtwAirAttacks;
     [SerializeField] private float _attackRange; // a circle used to detect enemies
     [SerializeField] private Transform _swordPoint; // the point at which the circle is drawn
     [SerializeField] private LayerMask _enemyLayers; // enemies we can hit within that circle
     private string[] _groundAttacks = {"GroundAttack1", "GroundAttack2", "GroundAttack3"};
+    private string[] _airAttacks = {"AirAttack1"/*, "AirAttack2", "AirAttack3"*/};
 
     [Header("Jumping")]
     [Range(2f, 10f)] [SerializeField] private float _fallMultiplier = 2.5f; // The gravity used to bring the player down after a long jump (long jump button press)
@@ -91,6 +95,12 @@ public class PlayerState : MonoBehaviour {
     }
     
     void Update() {
+
+        if (!_characterController2D.GetGrounded())
+            _canAirAttack = true;
+        else
+            _canAirAttack = false;
+
         CheckNonStateFunctions();
         CheckCurrentState();
         ChestInteraction();
@@ -103,7 +113,6 @@ public class PlayerState : MonoBehaviour {
         if (Input.GetButtonDown("XboxA"))
             _isJumping = true;
         
-
         // this is used to control how high the player can jump based off how long they hold the jump button
         if (_rigidBody2D.velocity.y < 0)
             _rigidBody2D.velocity += Vector2.up * Physics2D.gravity.y * (_fallMultiplier - 1) * Time.deltaTime;
@@ -124,6 +133,7 @@ public class PlayerState : MonoBehaviour {
     private void CheckNonStateFunctions() {
         DoubleJump();
         ExecuteGroundAttack();
+        ExecuteAirAttack();
         // AirborneGroundAttack();
     }
 
@@ -309,7 +319,22 @@ public class PlayerState : MonoBehaviour {
         _animator.SetTrigger(_groundAttacks[_randomGroundAttack]);
         yield return new WaitForSeconds(_timeBtwAttacks);
         _canAttack = true;
+    }
 
+    void ExecuteAirAttack() {
+        if (Input.GetButtonDown("XboxX") && !_characterController2D.GetGrounded() && _canAirAttack)
+            StartCoroutine(AirAttack());
+
+    }
+
+    IEnumerator AirAttack() {
+        int _randomAirAttack = Random.Range(0, _airAttacks.Length);
+        _canAirAttack = false;
+        _animator.SetBool("IsAirAttacking", true);
+        _animator.SetTrigger(_airAttacks[_randomAirAttack]);
+        yield return new WaitForSeconds(_timeBtwAirAttacks);
+        _canAirAttack = true;
+        _animator.SetBool("IsAirAttacking", false);
     }
 
     public void ScanForEnemies() {
@@ -317,7 +342,7 @@ public class PlayerState : MonoBehaviour {
 
           foreach(Collider2D _enemiesHit in _hitEnemies) {
 
-              _enemiesHit.GetComponent<Enemy>().TakeDamage(50); // damage the enemy
+              _enemiesHit.GetComponent<Enemy>().TakeDamage(_attackDamage); // damage the enemy
             //    StartCoroutine(cameraFollow.Shake(.15f, .15f)); // shake the camera for effect
             //    playArrSound(swordDamageList); // play a damage sound
           }
@@ -326,7 +351,6 @@ public class PlayerState : MonoBehaviour {
     void OnCollisionEnter2D(Collision2D _collisionInfo) {
         if (_collisionInfo.collider.name == "Spikes")
             TakeDamage(20, 200, 900);
-
     }
 
     bool ChestInteraction() {
