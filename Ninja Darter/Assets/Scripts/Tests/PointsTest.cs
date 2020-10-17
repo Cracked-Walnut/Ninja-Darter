e99e7094782c23b477e124157da170b0e50c7b2c
Fatih <maolud62@gmail.com>
2020-10-17 02:29:40 -0400

    Started development on test scripts

---
+++ Ninja Darter/Assets/Scripts/Tests/PointsTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests {
+
+    public class PointsTest {
+
+        // public GameObject _player;
+        // public Inventory _inventory;
+
+        void Awake() {
+            // _player = GameObject.Find("Player");
+            // Inventory _inventory = FindObjectOfType<Inventory>();
+        }
+        
+        // Run this test at the end of each level
+        [Test]
+        public void CalculateTotalPoints_Test() {
+            
+        }
+    }
+}
---
+++ Ninja Darter/Assets/Scripts/Tests/StageTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests {
+
+    public class StageTest {
+        
+        private GameObject _nodePrefab;
+
+        void Awake() {
+            _nodePrefab = GameObject.Find("SpawnNode");
+        }
+        
+        // A Test behaves as an ordinary method
+        [Test]
+        public void NodeIsSpawningRoom_Test() {
+            // Assert.AreEqual(1, 0);
+        }
+    }
+}
