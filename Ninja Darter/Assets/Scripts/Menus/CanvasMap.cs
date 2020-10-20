dd4e0c00ca600f4931f593f52de14e7198badc89
Fatih <maolud62@gmail.com>
2020-10-20 01:22:50 -0400

    Started development on an overworld map

---
+++ Ninja Darter/Assets/Scripts/Menus/CanvasMap.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasMap : MonoBehaviour {
+
+    [SerializeField] private GameObject _canvasMapCamera;
+    
+    void Start() {
+        _canvasMapCamera.SetActive(false);
+    }
+    
+    void Update() => CheckMapStatus();
+
+    void CheckMapStatus() {
+        if (Input.GetButtonDown("View (Back)"))
+            OpenCanvasMap();
+
+        if (OpenCanvasMap() && Input.GetButtonDown("XboxB"))
+            CloseCanvasMap();
+    }
+
+    bool OpenCanvasMap() {
+        _canvasMapCamera.SetActive(true);
+        return true;
+    }
+
+    void CloseCanvasMap() => _canvasMapCamera.SetActive(false);
+}
